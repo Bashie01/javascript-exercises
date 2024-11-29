ba174cb0c3326f5ad035b35c2801aaffb25e6f69
Bashie01 <aaron.jeffries@hotmail.co.uk>
2024-11-29 09:26:55 +0000

    first commit

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const yearDivByFour = year % 4 === 0;
+  const century = year % 100 === 0;
+  const yearDivByFourHundred = year % 400 === 0;
 
+  if (yearDivByFour && (!century || yearDivByFourHundred)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
